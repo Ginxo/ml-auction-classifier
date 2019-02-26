5cf6cc6ef9815a385447f20f23da7fb5d5a48587
Enrique Mingorance Cano <ginxaco@gmail.com>
2019-02-26 08:44:40 +0100

    KMeansAlgorithm 3d plot

--- ml_algorithms/KMeansAlgorithm.py
+++ ml_algorithms/KMeansAlgorithm.py
@@ -1,3 +1,5 @@
+import numpy as np
+
 import matplotlib.pyplot as plt
 from sklearn.cluster import KMeans
 from sklearn.decomposition import PCA
@@ -6,6 +8,7 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from domain.TechArticlesConstants import ENGLISH_STOP_WORDS, LABELS_COLOR_MAP
 from domain.WebInfoFactory import WebInfoFactory
 from service.FrequencySummarizer import FrequencySummarizer
+from mpl_toolkits import mplot3d  # can't be unimported, the 3d projection need it
 
 
 class KMeansAlgorithm(object):
@@ -14,6 +17,7 @@ class KMeansAlgorithm(object):
     def run(url, tech_articles, non_tech_articles):
         print('-----------------------------------------')
         print('---------- K-Means algorithm --------')
+        cluster_number = 5
 
         document_corpus = []
         for article in {**tech_articles, **non_tech_articles}.values():
@@ -25,23 +29,14 @@ class KMeansAlgorithm(object):
 
         # A K-means cluster is created. 15 clusters, by k-means++, 200 iterations,
         # https://scikit-learn.org/stable/modules/clustering.html#k-means
-        kmeans = KMeans(n_clusters=5, init='k-means++', max_iter=200, n_init=1, verbose=True)
+        kmeans = KMeans(n_clusters=cluster_number, init='k-means++', max_iter=100, n_init=1, verbose=True)
         kmeans.fit(vectorized)  # assign a label or cluster to each vector
 
-        keyword_clusters = {}
-        for i, cluster in enumerate(kmeans.labels_):
-            document = document_corpus[i]
-            summary = FrequencySummarizer().extract_features(document, 100)
-            if len(summary) == 100:
-                if cluster not in keyword_clusters:
-                    keyword_clusters[cluster] = set(summary)
-                else:
-                    keyword_clusters[cluster] = keyword_clusters[cluster].intersection(set(summary))
+        KMeansAlgorithm._print_clusters(kmeans, vectorizer, cluster_number)
 
-        print('Keyword Clusters {}'.format(keyword_clusters))
-        print('Document {} has this similarities {}'.format(url,
-                                                            KMeansAlgorithm._get_similarities(url, keyword_clusters)))
-        KMeansAlgorithm._plot(kmeans, vectorized)
+        prediction = KMeansAlgorithm._prediction(url, kmeans, vectorizer)
+        print('The url {} is in the cluster {}'.format(url, prediction))
+        KMeansAlgorithm._plot(kmeans, vectorized, prediction)
 
     @staticmethod
     def _get_similarities(url, keyword_clusters):
@@ -59,21 +54,50 @@ class KMeansAlgorithm(object):
         return len(set(article_summary_to_check).intersection(keyword_cluster)) * 100 / len(article_summary_to_check)
 
     @staticmethod
-    def _plot(kmeans, vectorized):
+    def _plot(kmeans, vectorized, prediction):
 
-        pca_num_components = 2
+        pca_num_components = 3
         reduced_data = PCA(n_components=pca_num_components).fit_transform(vectorized.todense())
-        fig, ax = plt.subplots()
+        ax = plt.axes(projection='3d')
+
+        clusters = {}
+        cluster_names = {}
         for index, instance in enumerate(reduced_data):
-            pca_comp_1, pca_comp_2 = reduced_data[index]
+            pca_comp_1, pca_comp_2, pca_comp_3 = reduced_data[index]
             color = LABELS_COLOR_MAP[kmeans.labels_[index]]
-            ax.scatter(pca_comp_1, pca_comp_2, c=color)
+            cluster = ax.scatter(pca_comp_1, pca_comp_2, pca_comp_3, c=color)
+            clusters[kmeans.labels_[index]] = cluster
+            cluster_names[kmeans.labels_[index]] = 'Cluster {}'.format(kmeans.labels_[index])
 
         print(kmeans.cluster_centers_)
         reduced_data = PCA(n_components=pca_num_components).fit_transform(kmeans.cluster_centers_)
         for index, instance in enumerate(reduced_data):
-            pca_comp_1, pca_comp_2 = reduced_data[index]
-            color = LABELS_COLOR_MAP[index] # the colours don't match with the label ones
-            ax.scatter(pca_comp_1, pca_comp_2, c=color, marker ='v')
+            pca_comp_1, pca_comp_2, pca_comp_3 = reduced_data[index]
+            # the colours don't match with the label ones
+            ax.scatter(pca_comp_1, pca_comp_2, pca_comp_3, c='r', marker='v')
 
+        plt.title('K-means flipante ')
+        plt.legend(clusters.values(), cluster_names.values())
+        plt.xticks(())
+        plt.yticks(())
         plt.show()
+
+    @staticmethod
+    def _print_clusters(kmeans, vectorizer, cluster_number):
+        print('Top terms per cluster:')
+        order_centroids = kmeans.cluster_centers_.argsort()[:, ::-1]
+        terms = vectorizer.get_feature_names()
+        for i in range(cluster_number):
+            print('Cluster {}'.format(i)),
+            for ind in order_centroids[i, :10]:
+                print('Cluster {}'.format(terms[ind])),
+            print('')
+
+    @staticmethod
+    def _prediction(url, kmeans: KMeans, vectorizer: TfidfVectorizer):
+        article_to_check = WebInfoFactory.url_to_web_info(url).get_words()
+        article_summary_to_check = FrequencySummarizer().extract_features(article_to_check, 25)
+        print('Prediction for these words {}'.format(article_summary_to_check))
+        Y = vectorizer.transform([" ".join(str(x) for x in article_summary_to_check)])
+        k_means_predicted = kmeans.predict(Y)
+        return k_means_predicted
